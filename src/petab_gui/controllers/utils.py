90078646790131984d8b6ab423b4cea8acdb90ed
PaulJonasJost <jost.pauljonas@gmail.com>
2025-03-08 14:42:29 +0100

    Unified settings to also include recent files

--- src/petab_gui/controllers/utils.py
+++ src/petab_gui/controllers/utils.py
@@ -1,6 +1,8 @@
 from PySide6.QtWidgets import QMessageBox, QMenu
-from PySide6.QtCore import QObject, Signal, QSettings
+from PySide6.QtCore import QObject, Signal
 from PySide6.QtGui import QAction
+
+from ..settings_manager import settings_manager
 
 
 def prompt_overwrite_or_append(controller):
@@ -29,8 +31,6 @@ class RecentFilesManager(QObject):
     def __init__(self, max_files=10):
         super().__init__()
         self.max_files = max_files
-        # TODO: link together with other settings, i.e. move settings to mc
-        self.settings = QSettings("PEtab_GUI", "PEtab_GUI")
         self.recent_files = self.load_recent_files()
         self.tool_bar_menu = QMenu("Recent Files")
         self.update_tool_bar_menu()
@@ -46,22 +46,23 @@ class RecentFilesManager(QObject):
 
     def load_recent_files(self):
         """Load recent files from settings."""
-        return self.settings.value("recent_files", [])
+        return settings_manager.get_value("recent_files", [])
 
     def save_recent_files(self):
         """Save recent files to settings."""
-        self.settings.setValue("recent_files", self.recent_files)
+        settings_manager.set_value("recent_files", self.recent_files)
 
     def update_tool_bar_menu(self):
-        """Create a menu for the tool bar."""
+        """Update the recent files menu."""
         self.tool_bar_menu.clear()
-        for idx, file_path in enumerate(self.recent_files):
+        for file_path in self.recent_files:
             action = QAction(file_path, self.tool_bar_menu)
-            action.triggered.connect(lambda: self.open_file.emit(file_path))
+            action.triggered.connect(lambda _, p=file_path: self.open_file.emit(p))  # Fix lambda issue
             self.tool_bar_menu.addAction(action)
         self.tool_bar_menu.addSeparator()
         clear_action = QAction("Clear Recent Files", self.tool_bar_menu)
         clear_action.triggered.connect(self.clear_recent_files)
+        self.tool_bar_menu.addAction(clear_action)
 
     def clear_recent_files(self):
         """Clear the recent files list."""
