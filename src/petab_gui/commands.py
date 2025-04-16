c9d147a371ea1aa0700f0be2ad456d0c031f0da5
PaulJonasJost <jost.pauljonas@gmail.com>
2025-04-16 15:39:25 +0200

    More stable undo redo of a singular column

--- src/petab_gui/commands.py
+++ src/petab_gui/commands.py
@@ -16,44 +16,31 @@ class ModifyColumnCommand(QUndoCommand):
         self.column_name = column_name
         self.add_mode = add_mode
         self.old_values = None
+        self.position = None
+
         if not add_mode and column_name in model._data_frame.columns:
+            self.position = model._data_frame.columns.get_loc(column_name)
             self.old_values = model._data_frame[column_name].copy()
 
     def redo(self):
         if self.add_mode:
             position = self.model._data_frame.shape[1]
-            self.model.beginInsertColumns(
-                QModelIndex(), position, position
-            )
+            self.model.beginInsertColumns(QModelIndex(), position, position)
             self.model._data_frame[self.column_name] = ""
             self.model.endInsertColumns()
         else:
-            position = list(
-                self.model._data_frame.columns
-            ).index(self.column_name)
-            self.model.beginRemoveColumns(
-                QModelIndex(), position, position
-            )
+            self.position = self.model._data_frame.columns.get_loc(self.column_name)
+            self.model.beginRemoveColumns(QModelIndex(), self.position, self.position)
             self.model._data_frame.drop(columns=self.column_name, inplace=True)
             self.model.endRemoveColumns()
 
     def undo(self):
         if self.add_mode:
-            position = list(
-                self.model._data_frame.columns
-            ).index(self.column_name)
-            self.model.beginRemoveColumns(
-                QModelIndex(), position, position
-            )
+            position = self.model._data_frame.columns.get_loc(self.column_name)
+            self.model.beginRemoveColumns(QModelIndex(), position, position)
             self.model._data_frame.drop(columns=self.column_name, inplace=True)
             self.model.endRemoveColumns()
         else:
-            position = self.model._data_frame.shape[1]
-            self.model.beginInsertColumns(
-                QModelIndex(), position, position
-            )
-            self.model._data_frame[self.column_name] = self.old_values
+            self.model.beginInsertColumns(QModelIndex(), self.position, self.position)
+            self.model._data_frame.insert(self.position, self.column_name, self.old_values)
             self.model.endInsertColumns()
-        self.model.beginRemoveColumns(
-            QModelIndex(), position, position
-        )
